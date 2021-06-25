e91a5f88f7aafabdf7cc956ca0a5e32830a7b7a1
Mary Rankin <mary-rankin@outlook.com>
2021-06-25 12:25:53 -0400

    formatting; added 2 projects

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const ejsMate = require('ejs-mate')
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects')
+})
+
+app.listen(3000, () => {
+    console.log('serving on port 3000')
+})
